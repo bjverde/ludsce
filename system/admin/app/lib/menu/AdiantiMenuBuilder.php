e0c753d03cfc4d03f0b3c8ec979c7c5e28412051
Bjverde <bjverde@yahoo.com.br>
2023-10-10 01:15:55 -0300

    :bug: #8 config temas

--- system/admin/app/lib/menu/AdiantiMenuBuilder.php
+++ system/admin/app/lib/menu/AdiantiMenuBuilder.php
@@ -59,6 +59,8 @@ class AdiantiMenuBuilder
                 return $menu_string;
                 break;
             case 'theme3':
+            case 'theme3_v4':
+            case 'theme3_v5':
                 ob_start();
                 $callback = array('SystemPermission', 'checkPermission');
                 $xml = new SimpleXMLElement(file_get_contents($file));
