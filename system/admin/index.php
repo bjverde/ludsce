35f4ac4f7f3e423e7bc08d7e5fc1a22d9135b136
Bjverde <bjverde@yahoo.com.br>
2024-01-26 01:39:14 -0300

    🔨 #14 update index.php - Adianti Fork Template 7.6.0.1

--- system/admin/index.php
+++ system/admin/index.php
@@ -9,45 +9,48 @@ $public = in_array($class, $ini['permission']['public_classes']);
 new TSession;
 ApplicationTranslator::setLanguage( TSession::getValue('user_language'), true );
 
-if ( TSession::getValue('logged') ){
-    if (isset($_REQUEST['template']) AND $_REQUEST['template'] == 'iframe'){
+if ( TSession::getValue('logged') )
+{
+    if (isset($_REQUEST['template']) AND $_REQUEST['template'] == 'iframe')
+    {
         $content = file_get_contents("app/templates/{$theme}/iframe.html");
     }else{
         $content = file_get_contents("app/templates/{$theme}/layout.html");
         $menu    = AdiantiMenuBuilder::parse('menu.xml', $theme);
         $content = str_replace('{MENU}', $menu, $content);
     }
-
-    //Novas linhas para Theme3_v4
-    $system_version = $ini['system']['version'];
+    
+    //Novas linhas para Theme3_v5
+    $system_version = $ini['system']['system_version'];
     $head_title  = $ini['system']['head_title'].' - v'.$system_version;
     $content     = str_replace('{head_title}', $head_title, $content);
     $content     = str_replace('{system_version}', $system_version, $content);
     $content     = str_replace('{logo-mini}', $ini['general']['application'], $content);
     $content     = str_replace('{logo-lg}', $ini['system']['logo-lg'], $content);
-    $content     = str_replace('{logo-link-class}', $ini['system']['logo-link-class'], $content);    
+    $content     = str_replace('{logo-link-class}', $ini['system']['logo-link-class'], $content);
 }else{
-    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1'){
+    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1')
+    {
         $content = file_get_contents("app/templates/{$theme}/public.html");
         $menu    = AdiantiMenuBuilder::parse('menu-public.xml', $theme);
         $content = str_replace('{MENU}', $menu, $content);
 
-        //Novas linhas para Theme3_v4
-        $system_version = $ini['system']['version'];
+        //Novas linhas para Theme3_v5
+        $system_version = $ini['system']['system_version'];
         $head_title  = $ini['system']['head_title'].' - v'.$system_version;
         $content     = str_replace('{head_title}', $head_title, $content);
         $content     = str_replace('{system_version}', $system_version, $content);
         $content     = str_replace('{logo-mini}', $ini['general']['application'], $content);
         $content     = str_replace('{logo-lg}', $ini['system']['logo-lg'], $content);
-        $content     = str_replace('{logo-link-class}', $ini['system']['logo-link-class'], $content);        
-    }else{
+        $content     = str_replace('{logo-link-class}', $ini['system']['logo-link-class'], $content);
+    }else {
         $content = file_get_contents("app/templates/{$theme}/login.html");
-        
-        //Novas linhas para Theme3_v4
-        $system_version = $ini['system']['version'];
+
+        //Novas linhas para Theme3_v5
+        $system_version = $ini['system']['system_version'];
         $head_title  = $ini['system']['head_title'].' - v'.$system_version;
         $content     = str_replace('{head_title}', $head_title, $content);
-        $content     = str_replace('{login-link}', $ini['system']['login-link'], $content);
+        $content     = str_replace('{login-link}', $ini['system']['login-link'], $content);        
     }
 }
 
@@ -56,17 +59,25 @@ $content = AdiantiTemplateParser::parse($content);
 
 echo $content;
 
-if (TSession::getValue('logged') OR $public){
-    if ($class){
+if (TSession::getValue('logged') OR $public)
+{
+    if ($class)
+    {
         $method = isset($_REQUEST['method']) ? $_REQUEST['method'] : NULL;
         AdiantiCoreApplication::loadPage($class, $method, $_REQUEST);
     }
-}else{
-    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1'){
-        if (!empty($ini['general']['public_entry'])){
+}
+else
+{
+    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1')
+    {
+        if (!empty($ini['general']['public_entry']))
+        {
             AdiantiCoreApplication::loadPage($ini['general']['public_entry'], '', $_REQUEST);
         }
-    }else{
+    }
+    else
+    {
         AdiantiCoreApplication::loadPage('LoginForm', '', $_REQUEST);
     }
 }
