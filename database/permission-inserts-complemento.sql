6afa8cf0cffa9e0d8f3ed4960408d975fe3e1c75
Bjverde <bjverde@yahoo.com.br>
2024-01-26 02:44:59 -0300

    :hammer: #14 melhorando script sql permission

--- database/permission-inserts-complemento.sql
+++ database/permission-inserts-complemento.sql
@@ -19,6 +19,27 @@ INSERT INTO system_group_program (id, system_group_id, system_program_id)
            , (SELECT p.id FROM system_program p where p.controller = 'ConfigForm')
            );
 
+-- Remove Documente
+DELETE FROM system_group_program 
+WHERE 1=1
+and system_group_id = 1 -- adm
+and system_program_id in (21,43,46,47,49);
+
+
+-- Remove Wiki
+DELETE FROM system_group_program 
+WHERE 1=1
+and system_group_id = 1 -- adm
+and system_program_id in (58,59,60,61);
+
+-- Remove Publicações
+DELETE FROM system_group_program 
+WHERE 1=1
+and system_group_id = 1 -- adm
+and system_program_id in (52,53,54,55,56);
+
+
+
 
 
 -- ------------------------
@@ -26,20 +47,39 @@ INSERT INTO system_group_program (id, system_group_id, system_program_id)
 INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Arquivos','ArquivosList');
 INSERT INTO system_group_program (id, system_group_id, system_program_id) 
     VALUES ( (SELECT max(sgp.id) + 1 FROM system_group_program as sgp)
-           , 2 -- adm
+           , 2 -- user
            , (SELECT p.id FROM system_program p where p.controller = 'ArquivosList')
            );
 
 INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Cadastro Arquivos','ArquivosForm');
 INSERT INTO system_group_program (id, system_group_id, system_program_id) 
     VALUES ( (SELECT max(sgp.id) + 1 FROM system_group_program as sgp)
-           , 2 -- adm
+           , 2 -- user
            , (SELECT p.id FROM system_program p where p.controller = 'ArquivosForm')
            );
 
 INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Config','ConfigForm');
 INSERT INTO system_group_program (id, system_group_id, system_program_id) 
     VALUES ( (SELECT max(sgp.id) + 1 FROM system_group_program as sgp)
-           , 2 -- adm
+           , 2 -- user
            , (SELECT p.id FROM system_program p where p.controller = 'ConfigForm')
            );
+
+-- Remove Documente
+DELETE FROM system_group_program 
+WHERE 1=1
+and system_group_id = 2 -- user
+and system_program_id in (21,43,46,47,49);
+
+
+-- Remove Wiki
+DELETE FROM system_group_program 
+WHERE 1=1
+and system_group_id = 2 -- user
+and system_program_id in (58,59,60,61);
+
+-- Remove Publicações
+DELETE FROM system_group_program 
+WHERE 1=1
+and system_group_id = 2 -- user
+and system_program_id in (52,53,54,55,56);
