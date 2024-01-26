e2bf86aaf4ed80590fee112a64c9110c2d5ff956
Bjverde <bjverde@yahoo.com.br>
2024-01-26 02:20:40 -0300

    :hammer: #14 melhorando script user

--- database/permission-inserts-complemento.sql
+++ database/permission-inserts-complemento.sql
@@ -18,3 +18,28 @@ INSERT INTO system_group_program (id, system_group_id, system_program_id)
            , 1 -- adm
            , (SELECT p.id FROM system_program p where p.controller = 'ConfigForm')
            );
+
+
+
+-- ------------------------
+-- Grupo user
+INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Arquivos','ArquivosList');
+INSERT INTO system_group_program (id, system_group_id, system_program_id) 
+    VALUES ( (SELECT max(sgp.id) + 1 FROM system_group_program as sgp)
+           , 2 -- adm
+           , (SELECT p.id FROM system_program p where p.controller = 'ArquivosList')
+           );
+
+INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Cadastro Arquivos','ArquivosForm');
+INSERT INTO system_group_program (id, system_group_id, system_program_id) 
+    VALUES ( (SELECT max(sgp.id) + 1 FROM system_group_program as sgp)
+           , 2 -- adm
+           , (SELECT p.id FROM system_program p where p.controller = 'ArquivosForm')
+           );
+
+INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Config','ConfigForm');
+INSERT INTO system_group_program (id, system_group_id, system_program_id) 
+    VALUES ( (SELECT max(sgp.id) + 1 FROM system_group_program as sgp)
+           , 2 -- adm
+           , (SELECT p.id FROM system_program p where p.controller = 'ConfigForm')
+           );
