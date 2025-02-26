9a550530d799ba4c9b8b7f83694863d8c375be57
Bjverde <bjverde@yahoo.com.br>
2025-02-26 01:58:13 -0300

    :hammer: #19 novos arquivos do adianti 8.0.0

---
+++ system/admin/app/config/application.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'general' =>  [
+        'timezone' => 'America/Sao_Paulo',
+        'language' => 'pt',
+        'application' => 'sample',
+        'title' => 'Adianti Framework 8.0',
+        'system_name_sub' => 'Fork',
+        'theme' => 'adminbs5_v2',
+        'debug' => '1'
+    ],
+    'template' => [
+        'navbar' => [
+            'has_menu_mode_switch' => '1',
+            'has_main_mode_switch' => '1'
+        ],
+        'dialogs' => [
+            'use_swal' => '0'
+        ],
+        'theme' => [
+            'menu_mode' => 'dark',
+            'main_mode' => 'light'
+        ]
+    ]
+];
