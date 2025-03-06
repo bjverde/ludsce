c531fec8af7b63781e75f0035ade6487efe8f1bf
Bjverde <bjverde@yahoo.com.br>
2025-03-06 03:10:23 -0300

    :abc: #19 formatando

--- system/admin/index.php
+++ system/admin/index.php
@@ -25,32 +25,23 @@ new TSession;
 ApplicationAuthenticationService::checkMultiSession();
 ApplicationTranslator::setLanguage( TSession::getValue('user_language'), true );
 
-if ( TSession::getValue('logged') )
-{
-    if (isset($_REQUEST['template']) AND $_REQUEST['template'] == 'iframe')
-    {
+if ( TSession::getValue('logged') ) {
+    if (isset($_REQUEST['template']) AND $_REQUEST['template'] == 'iframe') {
         $content = file_get_contents("app/templates/{$theme}/iframe.html");
-    }
-    else
-    {
+    } else {
         $content = file_get_contents("app/templates/{$theme}/layout.html");
         $content = str_replace('{MENU}', AdiantiMenuBuilder::parse('menu.xml', $theme), $content);
         $content = str_replace('{MENUTOP}', AdiantiMenuBuilder::parseNavBar('menu-top.xml', $theme), $content);
         $content = str_replace('{MENUBOTTOM}', AdiantiMenuBuilder::parseNavBar('menu-bottom.xml', $theme), $content);
     }
-}
-else
-{
-    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1')
-    {
+} else {
+    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1') {
         $content = file_get_contents("app/templates/{$theme}/public.html");
         $menu    = AdiantiMenuBuilder::parse('menu-public.xml', $theme);
         $content = str_replace('{MENU}', $menu, $content);
         $content = str_replace('{MENUTOP}', AdiantiMenuBuilder::parseNavBar('menu-top-public.xml', $theme), $content);
         $content = str_replace('{MENUBOTTOM}', AdiantiMenuBuilder::parseNavBar('menu-bottom-public.xml', $theme), $content);
-    }
-    else
-    {
+    } else {
         $content = file_get_contents("app/templates/{$theme}/login.html");
     }
 }
@@ -60,25 +51,17 @@ $content = AdiantiTemplateParser::parse($content);
 
 echo $content;
 
-if (TSession::getValue('logged') OR $public)
-{
-    if ($class)
-    {
+if (TSession::getValue('logged') OR $public){
+    if ($class) {
         $method = isset($_REQUEST['method']) ? $_REQUEST['method'] : NULL;
         AdiantiCoreApplication::loadPage($class, $method, $_REQUEST);
     }
-}
-else
-{
-    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1')
-    {
-        if (!empty($ini['general']['public_entry']))
-        {
+} else {
+    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1') {
+        if (!empty($ini['general']['public_entry'])) {
             AdiantiCoreApplication::loadPage($ini['general']['public_entry'], '', $_REQUEST);
         }
-    }
-    else
-    {
+    } else {
         AdiantiCoreApplication::loadPage('LoginForm', '', $_REQUEST);
     }
 }
