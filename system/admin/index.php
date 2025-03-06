46e03a2528be54a915b924cde27b139aa89ccef6
Bjverde <bjverde@yahoo.com.br>
2025-03-05 21:49:05 -0300

    :hammer: #19 update Adianti FrameWork  v8.0.0

--- system/admin/index.php
+++ system/admin/index.php
@@ -1,83 +1,38 @@
 <?php
 require_once 'init.php';
+
+$ini = AdiantiApplicationConfig::get();
 $theme  = $ini['general']['theme'];
-$class  = isset($_REQUEST['class']) ? $_REQUEST['class'] : '';
-$public = in_array($class, $ini['permission']['public_classes']);
-
-// AdiantiCoreApplication::setRouter(array('AdiantiRouteTranslator', 'translate'));
-
 new TSession;
-ApplicationTranslator::setLanguage( TSession::getValue('user_language'), true );
 
-if ( TSession::getValue('logged') )
-{
-    if (isset($_REQUEST['template']) AND $_REQUEST['template'] == 'iframe')
-    {
-        $content = file_get_contents("app/templates/{$theme}/iframe.html");
-    }else{
-        $content = file_get_contents("app/templates/{$theme}/layout.html");
-        $menu    = AdiantiMenuBuilder::parse('menu.xml', $theme);
-        $content = str_replace('{MENU}', $menu, $content);
-    }
-    
-    //Novas linhas para Theme3_v5
-    $system_version = $ini['system']['system_version'];
-    $head_title  = $ini['system']['head_title'].' - v'.$system_version;
-    $content     = str_replace('{head_title}', $head_title, $content);
-    $content     = str_replace('{system_version}', $system_version, $content);
-    $content     = str_replace('{logo-mini}', $ini['general']['application'], $content);
-    $content     = str_replace('{logo-lg}', $ini['system']['logo-lg'], $content);
-    $content     = str_replace('{logo-link-class}', $ini['system']['logo-link-class'], $content);
-}else{
-    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1')
-    {
-        $content = file_get_contents("app/templates/{$theme}/public.html");
-        $menu    = AdiantiMenuBuilder::parse('menu-public.xml', $theme);
-        $content = str_replace('{MENU}', $menu, $content);
+$content     = file_get_contents("app/templates/{$theme}/layout.html");
+$menu_string = AdiantiMenuBuilder::parse('menu.xml', $theme);
+$content     = ApplicationTranslator::translateTemplate($content);
+$content     = str_replace('{LIBRARIES}', file_get_contents("app/templates/{$theme}/libraries.html"), $content);
+$content     = str_replace('{class}', isset($_REQUEST['class']) ? $_REQUEST['class'] : '', $content);
+$content     = str_replace('{template}', $theme, $content);
+$content     = str_replace('{MENU}', $menu_string, $content);
+$content     = str_replace('{MENUTOP}', AdiantiMenuBuilder::parseNavBar('menu-top-public.xml', $theme), $content);
+$content     = str_replace('{MENUBOTTOM}', AdiantiMenuBuilder::parseNavBar('menu-bottom-public.xml', $theme), $content);
+$content     = str_replace('{lang}', $ini['general']['language'], $content);
+$content     = str_replace('{title}', $ini['general']['title'] ?? '', $content);
+$content     = str_replace('{template_options}',  json_encode($ini['template'] ?? []), $content);
+$content     = str_replace('{adianti_options}',  json_encode($ini['general']), $content);
 
-        //Novas linhas para Theme3_v5
-        $system_version = $ini['system']['system_version'];
-        $head_title  = $ini['system']['head_title'].' - v'.$system_version;
-        $content     = str_replace('{head_title}', $head_title, $content);
-        $content     = str_replace('{system_version}', $system_version, $content);
-        $content     = str_replace('{logo-mini}', $ini['general']['application'], $content);
-        $content     = str_replace('{logo-lg}', $ini['system']['logo-lg'], $content);
-        $content     = str_replace('{logo-link-class}', $ini['system']['logo-link-class'], $content);
-    }else {
-        $content = file_get_contents("app/templates/{$theme}/login.html");
+//--------------- adminbs5_t -----------------------
+$content     = str_replace('{system_name_sub}', $ini['general']['system_name_sub'] ?? '', $content);
+$content     = str_replace('{login}','usuario.logado', $content); //Aqui informe seu metodo de login
+//--------------- FIM adminbs5_t ------------------
 
-        //Novas linhas para Theme3_v5
-        $system_version = $ini['system']['system_version'];
-        $head_title  = $ini['system']['head_title'].' - v'.$system_version;
-        $content     = str_replace('{head_title}', $head_title, $content);
-        $content     = str_replace('{login-link}', $ini['system']['login-link'], $content);        
-    }
-}
 
-$content = ApplicationTranslator::translateTemplate($content);
-$content = AdiantiTemplateParser::parse($content);
+$css         = TPage::getLoadedCSS();
+$js          = TPage::getLoadedJS();
+$content     = str_replace('{HEAD}', $css.$js, $content);
 
 echo $content;
 
-if (TSession::getValue('logged') OR $public)
+if (isset($_REQUEST['class']))
 {
-    if ($class)
-    {
-        $method = isset($_REQUEST['method']) ? $_REQUEST['method'] : NULL;
-        AdiantiCoreApplication::loadPage($class, $method, $_REQUEST);
-    }
-}
-else
-{
-    if (isset($ini['general']['public_view']) && $ini['general']['public_view'] == '1')
-    {
-        if (!empty($ini['general']['public_entry']))
-        {
-            AdiantiCoreApplication::loadPage($ini['general']['public_entry'], '', $_REQUEST);
-        }
-    }
-    else
-    {
-        AdiantiCoreApplication::loadPage('LoginForm', '', $_REQUEST);
-    }
+    $method = isset($_REQUEST['method']) ? $_REQUEST['method'] : NULL;
+    AdiantiCoreApplication::loadPage($_REQUEST['class'], $method, $_REQUEST);
 }
--- system/admin/init.php
+++ system/admin/init.php
@@ -1,12 +1,7 @@
 <?php
-if (version_compare(PHP_VERSION, '7.4.0') == -1)
+if (version_compare(PHP_VERSION, '8.0.0') == -1)
 {
-    die ('The minimum version required for PHP is 7.4.0');
-}
-
-if (!file_exists('app/config/application.ini'))
-{
-    die('Application configuration file not found');
+    die ('The minimum version required for PHP is 8.0.0');
 }
 
 // define the autoloader
@@ -14,36 +9,14 @@ require_once 'lib/adianti/core/AdiantiCoreLoader.php';
 spl_autoload_register(array('Adianti\Core\AdiantiCoreLoader', 'autoload'));
 Adianti\Core\AdiantiCoreLoader::loadClassMap();
 
+// vendor autoloader
 $loader = require 'vendor/autoload.php';
 $loader->register();
 
-// read configurations
-$ini = parse_ini_file('app/config/application.ini', true);
-date_default_timezone_set($ini['general']['timezone']);
-AdiantiCoreTranslator::setLanguage( $ini['general']['language'] );
-ApplicationTranslator::setLanguage( $ini['general']['language'] );
-AdiantiApplicationConfig::load($ini);
-AdiantiApplicationConfig::apply();
+// apply app configurations
+AdiantiApplicationConfig::start();
 
 // define constants
-define('APPLICATION_NAME', $ini['general']['application']);
-define('OS', strtoupper(substr(PHP_OS, 0, 3)));
 define('PATH', dirname(__FILE__));
-define('LANG', $ini['general']['language']);
-
-//--- FORMDIN 5 START ---------------------------------------------------------
-FormDinHelper::verifyFormDinMinimumVersion($ini['system']['formdin_min_version']);
-FormDinHelper::verifyMinimumVersionAdiantiFrameWorkToSystem($ini['system']['adianti_min_version']);
-
-if(!defined('SYSTEM_VERSION') )  { define('SYSTEM_VERSION', $ini['system']['system_version']); }
-if(!defined('SYSTEM_NAME') )     { define('SYSTEM_NAME', $ini['general']['application']); }
-if(!defined('DS') )  { define('DS', DIRECTORY_SEPARATOR); }
-if(!defined('EOL') ) { define('EOL', "\n"); }
-if(!defined('ESP') ) { define('ESP', chr(32).chr(32).chr(32).chr(32) ); }
-if(!defined('TAB') ) { define('TAB', chr(9)); }
-//--- FORMDIN 5 END -----------------------------------------------------------
-
-// custom session name
-session_name('PHPSESSID_'.$ini['general']['application']);
 
 setlocale(LC_ALL, 'C');
